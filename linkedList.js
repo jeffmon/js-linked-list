88c47036705995f9279a29ce777753490b6cb464
jeffmon <jeffmon@gmail.com>
2017-05-25 16:18:35 -1000

    finished get head, tail, and add

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,55 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+  var head = null;
+  var tail = null;
 
+  function getHead(){
+    return head;
+  }
+
+  function add(value){
+    var node = {
+      value: value,
+      next: null
+    };
+    if(getHead() === null){
+      head = node;
+    }
+    else {
+      getTail().next = node;
+    }
+    tail = node;
+    return node;
+  }
+
+  function getTail(){
+    return tail;
+  }
+
+  function remove(num){
+
+  }
+
+  function get(num){
+
+  }
+
+  function insert(val, num){
+
+  }
+
+
+  return{
+    getHead,
+    add,
+    getTail,
+    remove,
+    get,
+    insert
+  };
 }
+
+var myLL = linkedListGenerator();
+myLL.add("Jeff");
+console.log(myLL.getHead());
