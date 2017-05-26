8221159fedb91e461610b20f59d4e4a572a16a42
jeffmon <jeffmon@gmail.com>
2017-05-25 19:13:18 -1000

    finished remove function

--- linkedList.js
+++ linkedList.js
@@ -3,6 +3,9 @@
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
+
+
+
 function linkedListGenerator(){
   var head = null;
   var tail = null;
@@ -22,9 +25,10 @@ function linkedListGenerator(){
     }
     else {
       getTail().next = node;
+      totalNodes++;
     }
     tail = node;
-    totalNodes++;
+
     return node;
   }
 
@@ -33,14 +37,31 @@ function linkedListGenerator(){
   }
 
   function remove(num){
+    var curNodeParent = get(num - 1);
+    var curNode = get(num);
 
+    if(num === 0){
+      head = head.next;
+    } else if(num > totalNodes){
+      return false;
+    } else if(num === totalNodes){
+      curNodeParent.next = null;
+      tail = curNodeParent;
+      totalNodes--;
+    }
+
+    else {
+      curNodeParent.next = curNode.next;
+      totalNodes--;
+      console.log("head", head);
+      console.log("tail", tail);
+    }
   }
 
   function get(num){
     var currentNode = head;
-    var nextNode = null;
     var i = 0;
-    if(num >= totalNodes){
+    if(num > totalNodes){
       return false;
     } else {
       while(i < num){
@@ -57,7 +78,6 @@ function linkedListGenerator(){
   }
 
 
-
   return{
     getHead,
     add,
@@ -70,4 +90,8 @@ function linkedListGenerator(){
 
 var myLL = linkedListGenerator();
 myLL.add("Jeff");
-console.log(myLL.getHead());
+myLL.add("max");
+myLL.add("ryu");
+myLL.add("ken");
+console.log(myLL.remove(3));
+console.log(JSON.stringify(myLL.getHead()));
